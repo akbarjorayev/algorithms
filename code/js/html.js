c1cbf724844a82854e2f56e705f4b06a86eea097
Akbar <akbarjonjorayev001@gmail.com>
2023-12-05 19:15:00 +0500

    JS

--- code/js/html.js
+++ code/js/html.js
@@ -1,9 +1,14 @@
 function add(htmlStr, pEl) {
-  const template = document.createElement('template')
+  if (typeof htmlStr != 'string' || !(pEl instanceof HTMLElement)) {
+    throw new Error('Invalid parameters')
+  }
 
+  const template = document.createElement('template')
   template.innerHTML += htmlStr.trim()
 
   const child = template.content.firstElementChild
+  if (!child) throw new Error('No valid HTML was provided')
+
   pEl.appendChild(child)
 
   return child
