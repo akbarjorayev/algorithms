d2e55d5a92882222a14e0a4c45282f9fec122748
Akbar <100585930+akbarjorayev@users.noreply.github.com>
2024-08-13 18:17:22 +0500

    Update image.js

--- code/js/image.js
+++ code/js/image.js
@@ -1,4 +1,4 @@
-export async function imageCompressor(
+export async function imageCompressorToData(
   image,
   maxWidth = 800,
   quality = 0.7,
@@ -39,6 +39,67 @@ export async function imageCompressor(
     reader.readAsDataURL(image)
   })
 }
+
+export async function imageCompressorToFile(
+  image,
+  maxWidth = 800,
+  quality = 0.7,
+  format = 'image/jpeg',
+  fileName = 'compressed_image.jpg'
+) {
+  return new Promise((res, rej) => {
+    const reader = new FileReader()
+
+    reader.onload = (event) => {
+      const img = new Image()
+      img.onload = () => {
+        const canvas = document.createElement('canvas')
+        const ctx = canvas.getContext('2d')
+
+        let width = img.width
+        let height = img.height
+
+        if (width > maxWidth) {
+          height = (height * maxWidth) / width
+          width = maxWidth
+        }
+
+        canvas.width = width
+        canvas.height = height
+
+        ctx.drawImage(img, 0, 0, width, height)
+
+        const dataUrl = canvas.toDataURL(format, quality)
+
+        const byteString = atob(dataUrl.split(',')[1])
+        const mimeString = dataUrl.split(',')[0].split(':')[1].split(';')[0]
+
+        const arrayBuffer = new ArrayBuffer(byteString.length)
+        const intArray = new Uint8Array(arrayBuffer)
+
+        for (let i = 0; i < byteString.length; i++) {
+          intArray[i] = byteString.charCodeAt(i)
+        }
+
+        const blob = new Blob([intArray], { type: mimeString })
+        const file = new File([blob], image?.name || fileName, {
+          type: mimeString,
+        })
+
+        console.log(file)
+        res(file)
+      }
+      img.src = event.target.result
+    }
+
+    reader.onerror = (err) => {
+      rej(err)
+    }
+
+    reader.readAsDataURL(image)
+  })
+}
+
 
 export function getImageBlob(file) {
   return new Promise((res, rej) => {
